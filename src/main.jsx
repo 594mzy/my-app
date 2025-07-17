59de1da825914c2bfecbaa2f0bae5bbfbcd25eac
mzy <671298798@qq.com>
2025-07-17 09:54:51 +0800

    translate Welcome Page

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import LoginPage from './components/LoginPage.jsx'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LoginPage />
-  </StrictMode>,
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+  </StrictMode>
 )
